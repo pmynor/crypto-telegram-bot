0336d4edae3b98d546f17ba93747b2a348d8fa2a
Mynor Portillo <mportillo@pronacom.org>
2026-03-10 14:46:44 -0600

    primer commit bot crypto

--- price.service.js
+++ price.service.js
@@ -5,9 +5,10 @@ async function getPrice(symbol) {
     const response = await axios.get(
       `https://api.binance.com/api/v3/ticker/price?symbol=${symbol}USDT`
     );
-
     return parseFloat(response.data.price);
   } catch (error) {
+    // ESTO ES CLAVE: Mira qué dice el log de Render
+    console.error(`Error en Binance API (${symbol}):`, error.response ? error.response.status : error.message);
     return null;
   }
 }
